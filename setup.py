885ac1d05ffbb15ea8e85af54d8654cca0764115
lchen <lchen@vps.server.local>
2021-08-15 14:43:34 +0000

    packaging support

--- setup.py
+++ setup.py
@@ -28,16 +28,22 @@ if sys.platform == 'win32':
                       ('_AFXDLL',1),
                      ]
 
+import os
+cwd = os.getcwd()
+
+cppfiles = ['txnaivebayes.cpp', 'pybrowser.cpp', 'pyhtql.cpp', 'pyhtqlmodule.cpp', 'pyhtqltools.cpp', 'txalign.cpp', 'txdict.cpp', 'txregex.cpp', 'docbase.cpp', 'htbrowser.cpp', 'HtPageModel.cpp', 'HtqlAdapter.cpp', 'htscript.cpp', 'htscriptgen.cpp', 'HtTextAlign.cpp', 'htwrapper.cpp', 'HtWrapperModels.cpp', 'htmlbuf.cpp', 'htmlql.cpp', 'htql.cpp', 'htqlexpr.cpp', 'htqlsyntax.cpp', 'htqlupdate.cpp', 'qhtmlql.cpp', 'qhtql.cpp', 'filedb.cpp', 'fileoper.cpp', 'perlfdb.cpp', 'recfile.cpp', 'txtset.cpp', 'alignment.cpp', 'bindata.cpp', 'btree.cpp', 'clustering.cpp', 'dirfunc.cpp', 'expr.cpp', 'fibheap.cpp', 'freemem.cpp', 'hmmMatrix.cpp', 'htwordlist.cpp', 'links.cpp', 'log.cpp', 'port.cpp', 'profilehmm.cpp', 'qlsyntax.cpp', 'referdata.cpp', 'referdatabase.cpp', 'referlink.cpp', 'referlink2.cpp', 'referlock.cpp', 'referscheduler.cpp', 'referset.cpp', 'refersqlparser.cpp', 'RegExParser.cpp', 'stack.cpp', 'stroper.cpp', 'strstack.cpp', 'tsoper.cpp', 'iwsqlsyntax.cpp', 'IrbNavigationBrowser.cpp', 'HTMLCacheFile.cpp', 'PathNavigationBrowser.cpp', 'PathNavigationBrowserControl.cpp', 'HtNaiveBayes.cpp']
+
 module1 = Extension('htql',
             define_macros = define_macros,
-            include_dirs = [get_python_inc() ],
+            include_dirs = [get_python_inc(), os.path.join(cwd, 'cpp') ],
             libraries = [],
             library_dirs = [],
-            sources = ["cpp/"+f for f in ['txnaivebayes.cpp', 'pybrowser.cpp', 'pyhtql.cpp', 'pyhtqlmodule.cpp', 'pyhtqltools.cpp', 'txalign.cpp', 'txdict.cpp', 'txregex.cpp', 'docbase.cpp', 'htbrowser.cpp', 'HtPageModel.cpp', 'HtqlAdapter.cpp', 'htscript.cpp', 'htscriptgen.cpp', 'HtTextAlign.cpp', 'htwrapper.cpp', 'HtWrapperModels.cpp', 'htmlbuf.cpp', 'htmlql.cpp', 'htql.cpp', 'htqlexpr.cpp', 'htqlsyntax.cpp', 'htqlupdate.cpp', 'qhtmlql.cpp', 'qhtql.cpp', 'filedb.cpp', 'fileoper.cpp', 'perlfdb.cpp', 'recfile.cpp', 'txtset.cpp', 'alignment.cpp', 'bindata.cpp', 'btree.cpp', 'clustering.cpp', 'dirfunc.cpp', 'expr.cpp', 'fibheap.cpp', 'freemem.cpp', 'hmmMatrix.cpp', 'htwordlist.cpp', 'links.cpp', 'log.cpp', 'port.cpp', 'profilehmm.cpp', 'qlsyntax.cpp', 'referdata.cpp', 'referdatabase.cpp', 'referlink.cpp', 'referlink2.cpp', 'referlock.cpp', 'referscheduler.cpp', 'referset.cpp', 'refersqlparser.cpp', 'RegExParser.cpp', 'stack.cpp', 'stroper.cpp', 'strstack.cpp', 'tsoper.cpp', 'iwsqlsyntax.cpp', 'IrbNavigationBrowser.cpp', 'HTMLCacheFile.cpp', 'PathNavigationBrowser.cpp', 'PathNavigationBrowserControl.cpp', 'HtNaiveBayes.cpp']]);
+            sources = ["cpp/"+f for f in cppfiles],
+        );
 
 setup (name = 'htql',
        version = '1.0',
-       description = 'This is a htql package',
+       description = 'This is an htql package',
        ext_modules = [module1])
 
 
